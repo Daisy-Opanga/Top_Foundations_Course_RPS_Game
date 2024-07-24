e962d4d7f2ea9a8cdc2a9455217ff8a4fed1eb5d
Daisy_Opanga <129028457+Daisy-Opanga@users.noreply.github.com>
2024-07-24 12:51:02 +0300

    Add files via upload

---
+++ rps_game.js
@@ -0,0 +1,98 @@
+let computerChoice, playerChoice;
+let computerScore = 0, playerScore = 0;
+let roundWinner , playerChoiceFinalCase, gameWinner;
+let  rounds ;
+
+function getComputerChoice(){
+   let randomChoice = Math.random();
+    if (randomChoice <= 1/3) {
+        return computerChoice = "Rock";
+    }
+    else if( randomChoice > 1/3 && randomChoice <= 2/3){
+        return computerChoice = "Paper";
+    }
+    else
+    return computerChoice = "Scissors";
+}
+function getPlayerChoice(){
+    playerChoice = prompt("Rock, Paper or Scissors?\n Please enter your choice.");
+    return playerChoiceFinalCase = playerChoice.toLowerCase();   
+}
+function playRound() {
+const computer = getComputerChoice();
+const player = getPlayerChoice();
+  switch (computer) {
+    case "Rock":
+      if (player === "paper") {
+        roundWinner  = "player";
+        playerScore = playerScore + 1;
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner  + " wins!"); 
+      } else if (player  === "scissors") {
+        roundWinner  = "computer";
+        computerScore = computerScore + 1;
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner  + " wins!"); 
+      } else if (player === "rock") {
+        roundWinner  = "You tied.";
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner);
+      } else return console.log("Incorrect input");
+     break;
+    case "Paper":
+      if (player === "rock") {
+        roundWinner  = "computer";
+        computerScore = computerScore + 1;
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner  + " wins!"); 
+      } else if (player === "scissors") {
+        roundWinner  = "player";
+        playerScore = playerScore + 1;
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner  + " wins!"); 
+      } else if (player === "paper") {
+        roundWinner  = "You tied.";
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner); 
+      } else return console.log("Incorrect input");
+     break;
+    case "Scissors":
+      if (player === "rock") {
+        roundWinner  = "player";
+        playerScore = playerScore + 1;
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner  + " wins!"); 
+      } else if (player === "paper") {
+        roundWinner  = "computer";
+        computerScore = computerScore + 1;
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner  + " wins!"); 
+      } else if (player === "scissors") {
+        roundWinner  = "You tied.";
+        return  console.log("The computer played " + computerChoice +
+            " and you played " + playerChoice + ". \n " + roundWinner); 
+      } else return console.log("Incorrect input");
+     break;
+    default:
+      console.log("An error occurred!");
+     break;
+  }
+}
+function playGame(){
+    for (let gameRound = 1;  gameRound <= 5; gameRound++) {
+         playRound();
+         console.log(" The current score is-> Player: " + playerScore + " & Computer: " + computerScore);
+    }
+    if (computerScore > playerScore){
+      gameWinner = "Computer";
+      console.log(" Too bad! You lost the game.");
+    }else if (playerScore > computerScore) {
+      gameWinner = "Player";
+      console.log("Congratulations! You won the game.");
+    } else {
+      console.log("Something went wrong!");
+      playGame();
+    }
+    return console.log("The final winner is " + gameWinner + ".");
+}
+playGame();
